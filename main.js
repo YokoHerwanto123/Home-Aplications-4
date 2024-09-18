0a24ce6bd65a30e22685c6092b429e25233fbcc4
YokoHerwanto123 <yokoherwanto205@gmail.com>
2024-09-18 18:13:17 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,116 @@
+function setDapurLed() {
+    ledDapur.style.backgroundColor = "red";
+    dapurLedImage.src = "./assets/lamp-on.png";
+}
+
+function setTerasLed() {
+    ledTeras.style.backgroundColor = "red";
+    terasLedImage.src = "./assets/lamp-on.png";
+}
+
+function setKamarLed() {
+    ledKamar.style.backgroundColor = "red";
+    kamarLedImage.src = "./assets/lamp-on.png";
+}
+
+function setTamuLed() {
+    ledTamu.style.backgroundColor = "red";
+    tamuLedImage.src = "./assets/lamp-on.png";
+}
+
+function setToiletLed() {
+    ledToilet.style.backgroundColor = "red";
+    toiletLedImage.src = "./assets/lamp-on.png";
+}
+
+function setKamartidurLed() {
+    kamarTidur.style.backgroundColor = "red";
+    kamartidurLedImage.src = "./assets/lamp-on.png";
+}
+
+
+/*function setFanone() {
+    fanone.style.backgroundColor = "red";
+    FanImageone.src = "./assets/fan-on.png";
+}
+
+function setFantwo() {
+    fantwo.style.backgroundColor = "red";
+    FanImagetwo.src = "./assets/fan-on.png";
+}*/
+
+
+
+function setValveOne() {
+    valveOne.style.backgroundColor = "red";
+    valveoneImage.src = "./assets/valve-on.png";
+}
+
+function setValveTwo() {
+    valveTwo.style.backgroundColor = "red";
+    valvetwoImage.src = "./assets/valve-on.png";
+}
+
+function setValveThree() {
+    valveThree.style.backgroundColor = "red";
+    valvethreeImage.src = "./assets/valve-on.png";
+}
+
+function setValveFour() {
+    valveFour.style.backgroundColor = "red";
+    valvefourImage.src = "./assets/valve-on.png";
+}
+
+
+function setMesinCuciOne() {
+    window.location.href = "wachingmachineone.html";
+}
+
+function setMesinCuciTwo() {
+    window.location.href = "wachingmachinetwo.html";
+}
+
+
+function setWashingmachineon() {
+    mesincucioneControl.style.backgroundColor = "red";
+    washingMachineImage.src = "./assets/mesincuci-on.png";
+}
+
+function setWashingmachintwo() {
+    mesincucitwoControl.style.backgroundColor = "red";
+    washingMachineImage.src = "./assets/mesincuci-on.png";
+}
+
+function setFanone() {
+    window.location.href = "fanone.html";
+}
+
+// Fungsi untuk mengarahkan ke halaman fantwo.html
+function setFantwo() {
+    window.location.href = "fantwo.html";
+}
+
+function setFanOne() {
+    fanControl.style.backgroundColor = "red";
+    fanOneImage.src = "./assets/fan-on.png";
+}
+
+function setFanTwo() {
+    fanControl.style.backgroundColor = "red";
+    fanTwoImage.src = "./assets/fan-on.png";
+}
+
+function speedSatu() {
+    Speedsatu.style.backgroundColor = "red";
+    SpeedSatu.src = "./assets/satu-on.png";
+}
+
+function speedDua() {
+    Speeddua.style.backgroundColor = "red";
+    SpeedDua.src = "./assets/dua-on.png";
+}
+
+function speedTiga() {
+    Speedtiga.style.backgroundColor = "red";
+    SpeedTiga.src = "./assets/dua-on.png";
+}
